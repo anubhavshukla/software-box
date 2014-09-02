a1c2b71e7e25ab887a99eb184941af1ac0942415
Anubhav Shukla <shukla.anubhav@gmail.com>
2014-09-02 16:17:24 +0530

    Added check for verification of repository folder before start of installation. If repository folder is not present, installation of the particular package exits.

--- components/7zip.cmd
+++ components/7zip.cmd
@@ -1,32 +1,45 @@
 REM This script will install 7-zip application 
+set APP_NAME=7-zip
+set APP_FOLDER=%SEVENZIP_FOLDER%
 
-call ./install_start.cmd 7-zip
+call ./install_start.cmd %APP_NAME%
 
-echo %msgPrefix%Verifying if 7-zip already installed
+echo %msgPrefix%Verifying access to repository location.
+echo %msgPrefix%Accessing location %SOFTWARE_DUMP_DIR%\%APP_FOLDER%.
+IF EXIST %SOFTWARE_DUMP_DIR%\%APP_FOLDER% ( 
+	echo %msgPrefix%Success! Continuing with installation.
+	echo.
+) ELSE ( 
+	echo %msgPrefix%Failure! Unable to access repository.
+	echo %msgPrefix%Exiting installation.
+	call ./install_failure.cmd %APP_NAME%
+	exit /B
+)
 
-IF EXIST %INSTALLATION_DIR%\%SEVENZIP_FOLDER% ( set isinstalled=Y) ELSE ( set isinstalled=N)
+echo %msgPrefix%Verifying if %APP_NAME% already installed
+IF EXIST %INSTALLATION_DIR%\%APP_FOLDER% ( set isinstalled=Y) ELSE ( set isinstalled=N)
 IF "%isinstalled%" == "Y" (
 	echo %msgPrefix%Software already installed.
 	IF "%OVERRIDE_OLD_INSTALLATION%"=="N" (
 		echo %msgPrefix%Override old installation flag set to N.
-		echo %msgPrefix%Skipping 7zip installation.
-		call ./install_skip.cmd 7-zip
+		echo %msgPrefix%Skipping %APP_NAME% installation.
+		call ./install_skip.cmd %APP_NAME%
 		exit /B
 	) ELSE (
 		echo %msgPrefix%Override old installation flag set to Y.
-		echo %msgPrefix%Deleting old 7zip installation.
-		call rd %INSTALLATION_DIR%\%SEVENZIP_FOLDER% /s /q
+		echo %msgPrefix%Deleting old %APP_NAME% installation.
+		call rd %INSTALLATION_DIR%\%APP_FOLDER% /s /q
 	)
 ) ELSE (
 	echo %msgPrefix%Software not installed
 )
 
-echo %msgPrefix%Copying 7zip directory from repository
+echo %msgPrefix%Copying %APP_NAME% directory from repository
 
-call robocopy %SOFTWARE_DUMP_DIR%\%SEVENZIP_FOLDER% %INSTALLATION_DIR%\%SEVENZIP_FOLDER% /s /NFL /NDL /nc /ns /np
+call robocopy %SOFTWARE_DUMP_DIR%\%APP_FOLDER% %INSTALLATION_DIR%\%APP_FOLDER% /s /NFL /NDL /nc /ns /np
 
 IF %errorlevel% neq 1 (
-	call ./install_failure.cmd 7-zip
+	call ./install_failure.cmd %APP_NAME%
 ) ELSE (
-	call ./install_success.cmd 7-zip
+	call ./install_success.cmd %APP_NAME%
 )
--- components/ant.cmd
+++ components/ant.cmd
@@ -1,11 +1,23 @@
 REM This script will install ANT application 
 set APP_NAME=ANT
+set APP_FOLDER=%ANT_FOLDER%
 
 call ./install_start.cmd %APP_NAME%
 
-echo %msgPrefix%Verifying if %APP_NAME% already installed
+echo %msgPrefix%Verifying access to repository location.
+echo %msgPrefix%Accessing location %SOFTWARE_DUMP_DIR%\%APP_FOLDER%.
+IF EXIST %SOFTWARE_DUMP_DIR%\%APP_FOLDER% ( 
+	echo %msgPrefix%Success! Continuing with installation.
+	echo.
+) ELSE ( 
+	echo %msgPrefix%Failure! Unable to access repository.
+	echo %msgPrefix%Exiting installation.
+	call ./install_failure.cmd %APP_NAME%
+	exit /B
+)
 
-IF EXIST %INSTALLATION_DIR%\%ANT_FOLDER% ( set isinstalled=Y) ELSE ( set isinstalled=N)
+echo %msgPrefix%Verifying if %APP_NAME% already installed
+IF EXIST %INSTALLATION_DIR%\%APP_FOLDER% ( set isinstalled=Y) ELSE ( set isinstalled=N)
 IF "%isinstalled%" == "Y" (
 	echo %msgPrefix%Software already installed.
 	IF "%OVERRIDE_OLD_INSTALLATION%"=="N" (
@@ -16,7 +28,7 @@ IF "%isinstalled%" == "Y" (
 	) ELSE (
 		echo %msgPrefix%Override old installation flag set to Y.
 		echo %msgPrefix%Deleting old %APP_NAME% installation.
-		call rd %INSTALLATION_DIR%\%ANT_FOLDER% /s /q
+		call rd %INSTALLATION_DIR%\%APP_FOLDER% /s /q
 	)
 ) ELSE (
 	echo %msgPrefix%Software not installed
@@ -24,14 +36,14 @@ IF "%isinstalled%" == "Y" (
 
 echo %msgPrefix%Copying %APP_NAME% directory from repository
 
-call robocopy %SOFTWARE_DUMP_DIR%\%ANT_FOLDER% %INSTALLATION_DIR%\%ANT_FOLDER% /s /NFL /NDL /nc /ns /np
+call robocopy %SOFTWARE_DUMP_DIR%\%APP_FOLDER% %INSTALLATION_DIR%\%APP_FOLDER% /s /NFL /NDL /nc /ns /np
 
 IF %errorlevel% neq 1 (
 	call ./install_failure.cmd %APP_NAME%
 ) ELSE (
-	echo %msgPrefix%Setting ANT_HOME to %INSTALLATION_DIR%\%ANT_FOLDER%
-	call setx ANT_HOME %INSTALLATION_DIR%\%ANT_FOLDER%
-	echo %msgPrefix%Adding %INSTALLATION_DIR%\%ANT_FOLDER%\bin folder to PATH variable
-	REM call setx PATH %PATH%;%INSTALLATION_DIR%\%ANT_FOLDER%\bin
+	echo %msgPrefix%Setting ANT_HOME to %INSTALLATION_DIR%\%APP_FOLDER%
+	call setx ANT_HOME %INSTALLATION_DIR%\%APP_FOLDER%
+	echo %msgPrefix%Adding %INSTALLATION_DIR%\%APP_FOLDER%\bin folder to PATH variable
+	REM call setx PATH %PATH%;%INSTALLATION_DIR%\%APP_FOLDER%\bin
 	call ./install_success.cmd %APP_NAME%
 )
